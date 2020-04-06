4010c3eb246c9d0d6fbb5a0037e9088e4adbefa0
theLittleGeek <huankimtran@gmail.com>
2020-04-05 23:48:34 -0500

    Done with BasicGUITimestamp, working!

---
+++ HuanGUI/src/BasicGUITimestamp/CMakeFiles/BasicGUITimeStamp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BasicGUITimeStamp.dir/main.cpp.o"
+  "BasicGUITimeStamp.pdb"
+  "BasicGUITimeStamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicGUITimeStamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HuanGUI/src/BasicGUITimestamp/CMakeFiles/ImagePtr2CVMat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImagePtr2CVMat.dir/main.cpp.o"
+  "ImagePtr2CVMat.pdb"
+  "ImagePtr2CVMat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImagePtr2CVMat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
