88bc970217cfc3c3776dd251c328a6d92d6cf3b0
Huan Tran <tran0966@umn.edu>
2020-04-18 19:59:59 +0100

    Adding changes

---
+++ HuanGUI/src/Linux/Testing_Utilities/OpenCV/Test_Video_Writer/CMakeFiles/VideoWriter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoWriter.dir/main.cpp.o"
+  "VideoWriter.pdb"
+  "VideoWriter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoWriter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
