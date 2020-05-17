8d32be519137a132c56d8d5b4829165cc206bc05
Huan Tran <tran0966@umn.edu>
2020-05-17 03:51:13 +0100

    Adding DLLs

---
+++ HuanGUI/lib/OpenCV_DLL/DebugUseDLL/install.bat
@@ -0,0 +1,46 @@
+copy opencv_calib3d430d.dll "%SystemRoot%\System32\"
+copy opencv_core430d.dll "%SystemRoot%\System32\"
+copy opencv_dnn430d.dll "%SystemRoot%\System32\"
+copy opencv_features2d430d.dll "%SystemRoot%\System32\"
+copy opencv_flann430d.dll "%SystemRoot%\System32\"
+copy opencv_gapi430d.dll "%SystemRoot%\System32\"
+copy opencv_highgui430d.dll "%SystemRoot%\System32\"
+copy opencv_imgcodecs430d.dll "%SystemRoot%\System32\"
+copy opencv_imgproc430d.dll "%SystemRoot%\System32\"
+copy opencv_ml430d.dll "%SystemRoot%\System32\"
+copy opencv_objdetect430d.dll "%SystemRoot%\System32\"
+copy opencv_photo430d.dll "%SystemRoot%\System32\"
+copy opencv_stitching430d.dll "%SystemRoot%\System32\"
+copy opencv_video430d.dll "%SystemRoot%\System32\"
+copy opencv_videoio430d.dll "%SystemRoot%\System32\"
+copy opencv_videoio_ffmpeg430_64.dll "%SystemRoot%\System32\"
+
+copy opencv_calib3d430.dll "%SystemRoot%\System32\"
+copy opencv_core430.dll "%SystemRoot%\System32\"
+copy opencv_dnn430.dll "%SystemRoot%\System32\"
+copy opencv_features2d430.dll "%SystemRoot%\System32\"
+copy opencv_flann430.dll "%SystemRoot%\System32\"
+copy opencv_gapi430.dll "%SystemRoot%\System32\"
+copy opencv_highgui430.dll "%SystemRoot%\System32\"
+copy opencv_imgcodecs430.dll "%SystemRoot%\System32\"
+copy opencv_imgproc430.dll "%SystemRoot%\System32\"
+copy opencv_ml430.dll "%SystemRoot%\System32\"
+copy opencv_objdetect430.dll "%SystemRoot%\System32\"
+copy opencv_photo430.dll "%SystemRoot%\System32\"
+copy opencv_stitching430.dll "%SystemRoot%\System32\"
+copy opencv_video430.dll "%SystemRoot%\System32\"
+copy opencv_videoio430.dll "%SystemRoot%\System32\"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
