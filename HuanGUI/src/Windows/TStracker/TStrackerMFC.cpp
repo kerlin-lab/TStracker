535f621178dd96d22c47c5d7cfb89ab1bc9eca85
Huan Tran <tran0966@umn.edu>
2020-06-05 05:33:44 +0100

    Added auto set process priority to real-time (does not work in win 10, process only gets set to High priority)

--- HuanGUI/src/Windows/TStracker/TStrackerMFC.cpp
+++ HuanGUI/src/Windows/TStracker/TStrackerMFC.cpp
@@ -37,6 +37,7 @@ CameraSelectionDlg* camSelectInitializer()
 
 BOOL TStrackerMain::InitInstance()
 {
+	// TODO 1: This setting real-time does not work in win10, is there  is way to fix?
 	// Making this process a Real-time process so that it receives the highest priority
 	SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
 
