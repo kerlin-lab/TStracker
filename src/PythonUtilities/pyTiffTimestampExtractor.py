65581ecd659e1c399afa77fdeb40f583df21af42
Huan Tran <huankimtran@gmail.com>
2020-09-23 13:44:35 -0500

    Trimming pyTiffTimestampExtractor instruction

--- src/PythonUtilities/pyTiffTimestampExtractor.py
+++ src/PythonUtilities/pyTiffTimestampExtractor.py
@@ -10,7 +10,7 @@ def extract_timestamp(file_name, tag_name='DateTime'):
 
 def extract_all_timestamp_from_folder(folder_path, file_name_structure, first_trial_index = 1, verbose = False):
   """
-    This function takes in a path to folder containing the tiff file and returns a dictionary contains timestamp of all the image in the folder
+    This function takes in a path to folder containing the tiff file and returns a dictionary contains timestamp of all the image, of which name matches the given strucutre, in the folder 
     @param folder_path: The path to the folder
     @param file_name_structure: The file name structure of image file inside the folder
       Ex: if the images inside the folder are
@@ -22,13 +22,14 @@ def extract_all_timestamp_from_folder(folder_path, file_name_structure, first_tr
       19144902_trial_ + TRIAL_NUMBER + .tiff
       to describe the structure to the function, replace where the TRAIL_NUMBER is at with 
       a pair of {}
-      With the above structure, the descritor will be
+      With the above structure, the descriptor will be
+
       19144902_trial_{}.tiff
 
       @ATTENTION: this function does not take into account files from different cameras with different serial number,
-      user need to do account for this by themself
+      user need to account for this by themself
       For example: In the folder /data/ , there are 3 images from camera 19144902 and 3 images from 19144903,
-      so the folder list looks like
+      so the file list looks like
         19144902_trial_1.tiff
         19144902_trial_2.tiff
         19144902_trial_3.tiff
@@ -40,7 +41,8 @@ def extract_all_timestamp_from_folder(folder_path, file_name_structure, first_tr
         When user calls extract_all_timestamp_from_folder('/data/', '19144902_trial_{}.tiff')
         this functions only extracts the timestamp from 19144902_trial_1.tiff to 19144902_trial_3.tiff
         but not 19144903_trial_1.tiff to 19144903_trial_3.tiff
-        If user wants to extract the remaining 3 files from 19144903 camera, he/she has to call this function another time like
+        If user wants to extract the remaining 3 files from 19144903 camera, he/she has to call this function another time with different file structure, like
+
         extract_all_timestamp_from_folder('/data/', '19144903_trial_{}.tiff')
 
     @return: 
